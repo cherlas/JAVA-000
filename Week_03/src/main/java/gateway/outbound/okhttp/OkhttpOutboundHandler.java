68a833569d4c56b9d190968576ef638854eb6d53
Cherlas <vm@li.cm>
2020-11-05 00:33:55 +0800

    第三周作业

--- Week_03/src/main/java/gateway/outbound/okhttp/OkhttpOutboundHandler.java
+++ Week_03/src/main/java/gateway/outbound/okhttp/OkhttpOutboundHandler.java
@@ -1,36 +1,35 @@
 package gateway.outbound.okhttp;
 
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpUtil;
+import kotlin.Pair;
 import okhttp3.*;
-import org.apache.http.util.EntityUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
+import java.util.Objects;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import static io.netty.handler.codec.rtsp.RtspResponseStatuses.OK;
 
 public class OkhttpOutboundHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(OkhttpOutboundHandler.class);
-
-    private final String proxyServer;
     private static OkHttpClient okHttpClient;
+    private final String proxyServer;
 
     public OkhttpOutboundHandler(String proxyServer) {
         this.proxyServer = proxyServer.endsWith("/") ? proxyServer.substring(0, proxyServer.length() - 1) : proxyServer;
         if (okHttpClient == null) {
             okHttpClient = new OkHttpClient();
         }
-    }
-
-    public void handle(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
-        String url = proxyServer + fullRequest.uri();
-        doRequest(fullRequest, ctx, url);
     }
 
     private static void doRequest(FullHttpRequest fullRequest, ChannelHandlerContext ctx, String url) {
@@ -43,15 +42,32 @@ public class OkhttpOutboundHandler {
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) {
-                if (fullRequest != null) {
-                    if (!HttpUtil.isKeepAlive(fullRequest)) {
-                        ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-                    } else {
-                        ctx.write(response);
+                FullHttpResponse response1 = null;
+                try {
+                    response1 = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(Objects.requireNonNull(response.body()).bytes()));
+                    for (Pair<? extends String, ? extends String> header : response.headers()) {
+                        response1.headers().add(header.component1(), header.component2());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    response1 = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+                    ctx.close();
+                } finally {
+                    if (fullRequest != null) {
+                        if (!HttpUtil.isKeepAlive(fullRequest)) {
+                            ctx.write(response1).addListener(ChannelFutureListener.CLOSE);
+                        } else {
+                            ctx.write(response1);
+                        }
                     }
                     ctx.flush();
                 }
             }
         });
+    }
+
+    public void handle(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        String url = proxyServer + fullRequest.uri();
+        doRequest(fullRequest, ctx, url);
     }
 }
