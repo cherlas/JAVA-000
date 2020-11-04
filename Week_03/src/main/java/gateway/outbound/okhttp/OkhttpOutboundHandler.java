c414ac5a4957dff88b63ab79cb70471c0a2365bc
Cherlas <vm@li.cm>
2020-11-04 23:00:25 +0800

    路由代码迁移

---
+++ Week_03/src/main/java/gateway/outbound/okhttp/OkhttpOutboundHandler.java
@@ -0,0 +1,14 @@
+package gateway.outbound.okhttp;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public class OkhttpOutboundHandler {
+    public OkhttpOutboundHandler(String proxyServer) {
+
+    }
+
+    public void handle(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+
+    }
+}
