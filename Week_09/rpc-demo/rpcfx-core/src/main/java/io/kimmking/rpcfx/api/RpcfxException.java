45deeb9a1f2a77ca547214f54ae9289b4c1a0dd4
Cherlas <vm@li.cm>
2020-12-18 23:22:27 +0800

    第9周作业1

---
+++ Week_09/rpc-demo/rpcfx-core/src/main/java/io/kimmking/rpcfx/api/RpcfxException.java
@@ -0,0 +1,31 @@
+package io.kimmking.rpcfx.api;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class RpcfxException extends RuntimeException {
+
+    private String errCode;
+
+    private Object data;
+
+    public RpcfxException(){
+        super();
+    }
+
+    public RpcfxException(String errMsg){
+        super(errMsg);
+    }
+
+    public RpcfxException(String errMsg, Throwable cas){
+        super(errMsg,cas);
+    }
+
+    public RpcfxException(String errCode,String errMsg, Object data){
+        super(errMsg);
+        this.errCode = errCode;
+        this.data = data;
+    }
+}
